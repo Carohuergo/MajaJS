90f6d1ffb17f85539a06a69952363021c06e5c63
Carolina Huergo <carohuergo@outlook.com>
2023-03-21 15:38:14 -0300

    grabo para correcion

--- js/java.js
+++ js/java.js
@@ -4,152 +4,150 @@ function multiplicar(prod1, prod2) {
 
 //base de datos de mis productos
 const productos = [
-  { cod: 101, modelo: "maria", precio: 3000 },
-  { cod: 102, modelo: "carolina", precio: 3400 },
-  { cod: 103, modelo: "paulina", precio: 4000 },
-  { cod: 104, modelo: "catalina", precio: 3200 },
+  {id: 1, modelo: "guiseppina", precio: 3000, descrip:"Confeccionado en paillette bordado sobre seda fría con spandex, tela importada.", img: "../assets/images/VestidoGuiseppina2.JPG", alt:"Vestido Guiseppina pailete rojo"},
+  {id: 2, modelo: "toia", precio: 3400, descrip:"Confeccionado en taftan de seda, escote en v con recortes y espalda con tiras cruzadas", img: "../assets/images/VestidoToia2.jpg", alt:"Vestido Toia taftan azul"},
+  {id: 3, modelo: "olivia", precio: 4000, descrip:"Vestido confeccionado en saten de seda, con corte americano, espalda baja y tajo.", img: "../assets/images/VestidoOliva2.PNG", alt:"vestido oliva seda verde" },
+  {id: 4, modelo: "alexia", precio: 3200, descrip:"Conjunto de top bando, pantalón suelto y saco Sastrero. Confeccionado en paillette plisado importado", img:"../assets/images/ConjuntoAlexia3.PNG", alt:"conjunto alexia paillette rosa" },
+  {id: 5, modelo: "maria", precio: 5000, descrip:"Vestido confeccionado en doble seda fría importada.", img:"../assets/images/VestidoMaria2.PNG", alt:"vestido maria seda negro"},
+  {id: 6, modelo: "milagros", precio: 5000, descrip:"Vestido confeccionado en paillette bordado sobre seda fría con spandex, tela importada.", img:"../assets/images/VestidoRosaconMangas.PNG", alt:"vestido rosa paillette rosa"}
 ];
 
 //array que se llenara de objetos(productos) que vaya ingresando el usuario
 const carrito = [];
 
-//le pregunto al usuario el modelo que quiere elegir
 
-//busca el modelo en la base de datos y lo agrega al carrito de compras. Si no existe, vuelve a preguntar
-let modeloinexistnte = "";
-function buscarModelo() {
-  do {
-    let productoElegido = prompt("Indique el modelo que desea alquilar");
-    let busqueda = productos.find(
-      (producto) => producto.modelo === productoElegido
-    );
-    if (busqueda === undefined) {
-      modeloinexistnte = "error";
-      alert("Modelo inexistente");
-    } else {
-      carrito.push(busqueda);
-      console.log(carrito);
-      alert("Modelo " + productoElegido + " fue ingresado al carrito")
-      modeloinexistnte = "";
+
+//Creo mis productos en HTML y relaciono el boton con el producto. 
+
+function relacionProductoConButton() {
+  //recorro mi array de productos
+  for (const producto of productos){
+      let adentroDelBotton = document.createElement("div")
+      //armo el html que relacionara cada producto con el boton
+      //adentro esta la estructura del html del producto y un modal para avisarle al usuario que se agrego al producto
+      adentroDelBotton.innerHTML = `
+      <div class="row margenGaleria">
+        <div class="col-sm-6">
+          <div class="ih-item square effect6 from_top_and_bottom"><a>
+            <div class="imgGaleria ">
+             <img src="${producto.img}"  alt="${producto.alt}" loading="lazy">
+            </div>
+            <div class="info">
+              <h3> ${producto.modelo} </h3>
+              <h3>$ ${producto.precio}</h3>
+              <p> ${producto.descrip} </p>
+              <div class="d-flex justify-content-center">
+                <button type="button" class="btn btn-dark btn-outline-light" id=${producto.id} data-bs-toggle="modal" data-bs-target="#exampleModal" >Agregar al carrito</button>
+              </div>
+            </div></a>
+          </div>
+        </div>
+      </div>
+      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-header agrandar">
+            <h1 class="modal-title" id="exampleModalLabel">Carrito de Compra</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Cerrar"></button>
+          </div>
+          <div class="modal-body agrandar">
+            Producto agregado exitosamente al carrito
+          </div>
+          <div class="modal-footer">
+          <button
+          class="btn btn-dark btn-outline-ligh"
+          type="button"
+          data-bs-toggle="offcanvas"
+          data-bs-target="#offcanvasScrolling"
+          aria-controls="offcanvasScrolling"
+        >
+          VER CARRITO
+        </button>
+          </div>
+        </div>
+      </div>
+    </div>
+      
+        `
+        let agregoParraffo = document.getElementById('galeriaDeProductos')
+        agregoParraffo.append(adentroDelBotton)
+        let botonParaAgregar = document.getElementById(`${producto.id}`)
+        botonParaAgregar.addEventListener('click',agregarAlCarrito)
+        //botonAgregarCarrito.innerText = "Agregado al carrito"
     }
-  } while (modeloinexistnte === "error");
-}
-
-//evaluo si desea agregar mas modelos al carrito y agrego.
-function elegirMasProductos() {
-  let segundoProducto = prompt("¿Desea agregar otro modelo (si/no)? ");
-  while (segundoProducto === "si") {
-    buscarModelo();
-    segundoProducto = prompt("Desea agregar otro modelo (si/no)? ");
   }
+
+//funcion para agregar el producto al carrito y sumar cantidades
+function agregarAlCarrito(e){
+  let id = Number(e.target.getAttribute('id'))
+  let productoAAgregar = productos.find((producto)=>producto.id === id)
+  if(existeEnCarrito(productoAAgregar)){
+      let productoEnCarrito = carrito.find((producto)=>producto.id === productoAAgregar.id)
+      let indiceDelProducto = carrito.indexOf(productoEnCarrito)
+      carrito[indiceDelProducto].cantidad+=1
+      let productoAnterior = document.getElementById(`producto${productoAAgregar.id}`)
+      productoAnterior.innerHTML = `
+          <div class="cardCarrito">
+          <h2>${productoAAgregar.modelo.toUpperCase()}</h2>
+          <h3>$ ${productoAAgregar.precio}</h3>
+          <h3>Cantidad: ${carrito[indiceDelProducto].cantidad}</h3>
+          <h3>Total de ${productoAAgregar.modelo} : $ ${carrito[indiceDelProducto].cantidad*productoAAgregar.precio}</h3>
+          </div>
+      `
+
+  }else{
+      let productoConCantidad = {...productoAAgregar,cantidad:1}
+      carrito.push(productoConCantidad)
+      let cardCarrito = document.createElement('div')
+      cardCarrito.innerHTML = `
+          <div class="cardCarrito">
+          <h2>${productoConCantidad.modelo.toUpperCase()}</h2>
+          <h3>$ ${productoConCantidad.precio}</h3>
+          <h3>Cantidad: 1</h3>
+          </div>
+
+          
+      `
+      cardCarrito.setAttribute('id',`producto${productoConCantidad.id}`)
+      let carritoContendor = document.getElementById('carrito')
+      carritoContendor.append(cardCarrito)
+  }
+
 }
-
-//funcion para eliminar un producto del carrito
-
-function eliminarProducto() {
-  let respuesta = prompt("Desea eliminar un producto del carrito (si/no)?");
-  if (respuesta === "si") {
-    do {
-      let modeloEliminar = prompt(
-        "Indique el modelo que desea eliminar del carrito de compras"
-      );
-      let busquedaModeloEliminar = carrito.find(
-        (producto) => producto.modelo === modeloEliminar
-      );
-      if (busquedaModeloEliminar === undefined) {
-        modeloinexistnte = "error";
-        alert("Modelo inexistente");
-      } else {
-        let posicion = carrito.indexOf(busquedaModeloEliminar);
-        console.log(posicion);
-        carrito.splice(posicion, 1);
-        console.log(carrito);
-        alert("Se elimino del carrito al modelo: " + modeloEliminar);
-        modeloinexistnte = "";
+//verifico si esta el producto en el carrito para luego ver si lo creo por primera vez o le cambio la cantidad
+function existeEnCarrito(productoAChequear){
+  for (producto of carrito){
+      if(productoAChequear.id === producto.id){
+          return true
       }
-    } while (modeloinexistnte === "error");
   }
+  return false
 }
 
-//mostramos el carrito final al usuario
-function carritoFinal() {
-  const modelosDelCarritoFinal = carrito.map((producto) => producto.modelo);
-  alert("Carrito de compra final = " + modelosDelCarritoFinal);
-}
 
 //sumamos el total el carrito//
-let totalProducto = 0;
-function sumaCarrito() {
-  carrito.forEach((producto) => {
-    totalProducto += producto.precio;
-  });
-  alert("El precio final de su carrito de compra es " + totalProducto);
-}
-//final de compra. pasamos a forma de pagos//
-function metodosDePago() {
-  alert(
-    "Puede abonar con tarjeta hasta 3 cuotas con recargo o en efectivo con un 10% de descuento"
-  );
 
-  let formaDePago = prompt("¿Como va a abonar, en efectivo o con tarjeta?");
-  let cuota = 0;
+function calcularTotal() {
+  let totalProducto = 0
+    carrito.forEach((producto) => {
+      totalProducto += (producto[precio])*(producto[cantidad]);
+      console.log(totalProducto)
+    });
 
-  if (formaDePago == "efectivo") {
-    let precioFinal = multiplicar(totalProducto, 0.9);
-    alert("El total a abonar es " + precioFinal);
-  } else {
-    while (cuota != 1 && cuota != 2 && cuota != 3) {
-      cuota = parseInt(
-        prompt(
-          "¿Ingrese en cuantas cuotas desea abonar: 1 (+6%), 2(+10%) o 3(+15%) cuotas?"
-        )
-      );
-    }
-    if (cuota === 1) {
-      let unPago = multiplicar(totalProducto, 1.06);
-      alert("El total a abonar en una cuota es " + Math.round(unPago));
-    } else if (cuota === 2) {
-      let dosPagos = multiplicar(totalProducto, 1.1);
-      alert("El total a abonar en dos cuota es " + Math.round(dosPagos));
-    } else {
-      let tresPago = multiplicar(totalProducto, 1.15);
-      alert("El total a abonar en tres cuotas es " + Math.round(tresPago));
-    }
-  }
+  sumarTotal= document.createElement("div")
+  sumarTotal.innerHTML =`
+  <div class="cardCarrito">
+  <h3> El total del carrito es $ ${totalProducto}</h3>
+  </div>
+`
+let contenedorTotalCarrito= document.getElementById("totalCarrito")
+contenedorTotalCarrito.append(sumarTotal)
 }
 
-//Creo un objetos, le pido datos al usuario y los guardo en base de datos 
 
-function datosCliente () {
-    class Cliente {
-        constructor (nombre, apellido, dni, telefono, mail, fecha) {
-            this.nombre = nombre;
-            this.apellido = apellido;
-            this.dni = dni;
-            this.telefono = telefono;
-            this.mail = mail;
-            this.fecha = fecha
-        }
-
-    }
-   let nombre=prompt("Ingrese su nombre")
-   let apellido=prompt("Ingrese su apelldo")
-   let dni = prompt("Ingrese su dni")
-   let telefono =prompt("Ingrese su telefono")
-   let mail= prompt ("Ingrese su mail")
-   let fecha = prompt("Ingrese la fecha del evento (dd/mm/aa)")
-
-
-    const cliente1 = new Cliente (nombre, apellido, dni, telefono, mail, fecha)
-    const BaseDeDatosClientes = [cliente1]
-    console.log(BaseDeDatosClientes)
-    alert("verificaremos sus datos y si los modelos estan libres para la fecha indicada, le llegara un mail con los datos para efectuar el pago. Gracias, equipo Maja")
-}
 //llamo a las funciones creadas.
-buscarModelo();
-elegirMasProductos();
-eliminarProducto();
-carritoFinal();
-sumaCarrito();
-metodosDePago();
-datosCliente ()
+relacionProductoConButton();
+calcularTotal();
+
+
+
---
+++ js/javaAnterior.js
@@ -0,0 +1,162 @@
+function multiplicar(prod1, prod2) {
+    return prod1 * prod2;
+  }
+  
+  //base de datos de mis productos
+  const productos = [
+    {id: 1, modelo: "guiseppina", precio: 3000, descrip:"Confeccionado en paillette bordado sobre seda fría con spandex, tela importada.", img: "../assets/images/VestidoGuiseppina2.JPG", alt:"Vestido Guiseppina pailete rojo"},
+    {id: 2, modelo: "toia", precio: 3400, descrip:"Confeccionado en taftan de seda, escote en v con recortes y espalda con tiras cruzadas", img: "../assets/images/VestidoToia2.jpg", alt:"Vestido Toia taftan azul"},
+    {id: 3, modelo: "olivia", precio: 4000, descrip:"Vestido confeccionado en saten de seda, con corte americano, espalda baja y tajo.", img: "../assets/images/VestidoOliva2.PNG", alt:"vestido oliva seda verde" },
+    {id: 4, modelo: "alexia", precio: 3200, descrip:"Conjunto de top bando, pantalón suelto y saco Sastrero. Confeccionado en paillette plisado importado", img:"../assets/images/ConjuntoAlexia3.PNG", alt:"conjunto alexia paillette rosa" },
+    {id: 5, modelo: "maria", precio: 5000, descrip:"Vestido confeccionado en doble seda fría importada.", img:"../assets/images/VestidoMaria2.PNG", alt:"vestido maria seda negro"},
+    {id: 6, modelo: "milagros", precio: 5000, descrip:"Vestido confeccionado en paillette bordado sobre seda fría con spandex, tela importada.", img:"../assets/images/VestidoRosaconMangas.PNG", alt:"vestido rosa paillette rosa"}
+  ];
+  
+  //array que se llenara de objetos(productos) que vaya ingresando el usuario
+  const carrito = [];
+  
+  
+  //busca el modelo en la base de datos y lo agrega al carrito de compras. Si no existe, vuelve a preguntar
+  //let modeloinexistnte = "";
+  function relacionProductoConButton() {
+    do {
+      let productoElegido = prompt("Indique el modelo que desea alquilar");
+      let busqueda = productos.find(
+        (producto) => producto.modelo === productoElegido
+      );
+      if (busqueda === undefined) {
+        modeloinexistnte = "error";
+        alert("Modelo inexistente");
+      } else {
+        carrito.push(busqueda);
+        console.log(carrito);
+        alert("Modelo " + productoElegido + " fue ingresado al carrito")
+        modeloinexistnte = "";
+    } 
+}while (modeloinexistnte === "error");
+  }
+  
+  
+  
+  
+  //evaluo si desea agregar mas modelos al carrito y agrego.
+  function elegirMasProductos() {
+    let segundoProducto = prompt("¿Desea agregar otro modelo (si/no)? ");
+    while (segundoProducto === "si") {
+      buscarModelo();
+      segundoProducto = prompt("Desea agregar otro modelo (si/no)? ");
+    }
+  }
+  
+  // //funcion para eliminar un producto del carrito
+  
+  function eliminarProducto() {
+    let respuesta = prompt("Desea eliminar un producto del carrito (si/no)?");
+    if (respuesta === "si") {
+      do {
+        let modeloEliminar = prompt(
+          "Indique el modelo que desea eliminar del carrito de compras"
+        );
+        let busquedaModeloEliminar = carrito.find(
+          (producto) => producto.modelo === modeloEliminar
+        );
+        if (busquedaModeloEliminar === undefined) {
+          modeloinexistnte = "error";
+          alert("Modelo inexistente");
+        } else {
+          let posicion = carrito.indexOf(busquedaModeloEliminar);
+          console.log(posicion);
+          carrito.splice(posicion, 1);
+          console.log(carrito);
+          alert("Se elimino del carrito al modelo: " + modeloEliminar);
+          modeloinexistnte = "";
+        }
+      } while (modeloinexistnte === "error");
+    }
+  }
+
+  
+  // //mostramos el carrito final al usuario
+  function carritoFinal() {
+    const modelosDelCarritoFinal = carrito.map((producto) => producto.modelo);
+    alert("Carrito de compra final = " + modelosDelCarritoFinal);
+  }
+  
+  // //sumamos el total el carrito//
+  let totalProducto = 0;
+  function sumaCarrito() {
+    carrito.forEach((producto) => {
+      totalProducto += producto.precio;
+    });
+    alert("El precio final de su carrito de compra es " + totalProducto);
+  }
+  //final de compra. pasamos a forma de pagos//
+  function metodosDePago() {
+    alert(
+      "Puede abonar con tarjeta hasta 3 cuotas con recargo o en efectivo con un 10% de descuento"
+    );
+  
+    let formaDePago = prompt("¿Como va a abonar, en efectivo o con tarjeta?");
+    let cuota = 0;
+  
+    if (formaDePago == "efectivo") {
+      let precioFinal = multiplicar(totalProducto, 0.9);
+      alert("El total a abonar es " + precioFinal);
+    } else {
+      while (cuota != 1 && cuota != 2 && cuota != 3) {
+        cuota = parseInt(
+          prompt(
+            "¿Ingrese en cuantas cuotas desea abonar: 1 (+6%), 2(+10%) o 3(+15%) cuotas?"
+          )
+        );
+      }
+      if (cuota === 1) {
+        let unPago = multiplicar(totalProducto, 1.06);
+        alert("El total a abonar en una cuota es " + Math.round(unPago));
+      } else if (cuota === 2) {
+        let dosPagos = multiplicar(totalProducto, 1.1);
+        alert("El total a abonar en dos cuota es " + Math.round(dosPagos));
+      } else {
+        let tresPago = multiplicar(totalProducto, 1.15);
+        alert("El total a abonar en tres cuotas es " + Math.round(tresPago));
+      }
+    }
+  }
+  
+  // //Creo un objetos, le pido datos al usuario y los guardo en base de datos 
+  
+  function datosCliente () {
+      class Cliente {
+          constructor (nombre, apellido, dni, telefono, mail, fecha) {
+              this.nombre = nombre;
+              this.apellido = apellido;
+              this.dni = dni;
+              this.telefono = telefono;
+              this.mail = mail;
+              this.fecha = fecha
+          }
+  
+      }
+     let nombre=prompt("Ingrese su nombre")
+     let apellido=prompt("Ingrese su apelldo")
+     let dni = prompt("Ingrese su dni")
+     let telefono =prompt("Ingrese su telefono")
+     let mail= prompt ("Ingrese su mail")
+     let fecha = prompt("Ingrese la fecha del evento (dd/mm/aa)")
+  
+  
+      const cliente1 = new Cliente (nombre, apellido, dni, telefono, mail, fecha)
+      const BaseDeDatosClientes = [cliente1]
+      console.log(BaseDeDatosClientes)
+      alert("verificaremos sus datos y si los modelos estan libres para la fecha indicada, le llegara un mail con los datos para efectuar el pago. Gracias, equipo Maja")
+  }
+
+  //llamo a las funciones creadas.
+  elegirMasProductos();
+  eliminarProducto();
+  carritoFinal();
+  sumaCarrito();
+  metodosDePago();
+  datosCliente ()
+  
+  
